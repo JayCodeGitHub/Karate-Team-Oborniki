fcefe28bd550feb3f2f0a97b87b85814d8e19cca
Jakubek5338 <32836419+Jakubek5338@users.noreply.github.com>
2020-07-01 13:08:14 +0200

    change autor

--- src/components/fotter/fotter.js
+++ src/components/fotter/fotter.js
@@ -58,7 +58,7 @@ const Fotter = () => (
     <FooterWrapper>
         <ContaktData>
         </ContaktData>
-        <Author><h1>Strona Autorstwa <span className="red">Jakuba Gryzińskiego</span></h1></Author>
+        <Author><h1>Created By <span className="red">Jakub G</span></h1></Author>
         <ContaktDataRight>
         </ContaktDataRight>
     </FooterWrapper>
